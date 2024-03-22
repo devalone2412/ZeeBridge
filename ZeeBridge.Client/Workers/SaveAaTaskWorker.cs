1f369feca6fb4666b31e199afea03cab8d4fb431
devbrownbear2412 <devbrownbear@gmail.com>
2024-03-22 15:43:02 +0700

    Support Error boundary event

---
+++ ZeeBridge.Client/Workers/SaveAaTaskWorker.cs
@@ -0,0 +1,23 @@
+﻿using Zeebe.Client.Api.Responses;
+using ZeeBridge.Attributes;
+using ZeeBridge.Client.Models;
+using ZeeBridge.Extenstion;
+using ZeeBridge.Interfaces;
+
+namespace ZeeBridge.Client.Workers;
+
+[JobType("save-aa-task")]
+public class SaveAaTaskWorker : IJobWorkerHandlerAsync
+{
+    public Task HandlerAsync(IJob activatedJob, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Save AA task worker activated");
+        var data = new WeatherRequest
+        {
+            City = "Ho Chi Minh"
+        };
+        
+        activatedJob.MarkAsCompleted(data);
+        return Task.CompletedTask;
+    }
+}
---
+++ ZeeBridge/Exceptions/BpmnErrorEventException.cs
@@ -0,0 +1,18 @@
+﻿namespace ZeeBridge.Exceptions;
+
+public class BpmnErrorEventException : Exception
+{
+    public string Code { get; }
+    public object? ErrorData { get; set; }
+
+    public BpmnErrorEventException(string? message, string code) : base(message)
+    {
+        Code = code;
+    }
+
+    public BpmnErrorEventException(string? message, string code, object? errorData) : base(message)
+    {
+        Code = code;
+        ErrorData = errorData;
+    }
+}
